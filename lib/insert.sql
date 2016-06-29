820f398960cf3422e39c405c3ee596ca7c184125
Rigz777 <rigzgoodmusic@gmail.com>
2016-06-29 03:22:30 -0400

    done

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
+("Mr. Chocolate", 13, "M", "Brown", "Calm", "True"),
+("Rowdy", 7, "M", "Red", "Hyper", "True"),
+("Tabitha", 20, "F", "White", "Calm", "False"),
+("Sergeant Brown", 100, "M", "Brown", "Angry", "False"),
+("Melissa", 4, "F", "White", "Timid", "True"),
+("Grinch", 11, "M", "Green", "Moody", "True"),
+("Wendy", 13, "F", "Black", "Calm", "True"),
+(NULL, 21, "M", "Black", "Angry", "True");
